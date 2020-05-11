981d3f6fc9966d64dc0e7953b4ae8b68bdd6dc7a
joshiyugal19 <joshiyugal1999@gmail.com>
2020-05-11 13:08:21 +0530

    Updates

---
+++ precache-manifest.5a3fd7c4b5f3d8081f973b56150e5e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04efbca1dc1613efcb8768cb3d69e74",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "a9a4f76190c019c48ab8",
+    "url": "/corona-tracker/static/css/main.4563a426.chunk.css"
+  },
+  {
+    "revision": "fbc9b8011e7dbd64a144",
+    "url": "/corona-tracker/static/js/2.7e3969e8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.7e3969e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a4f76190c019c48ab8",
+    "url": "/corona-tracker/static/js/main.8d0db45e.chunk.js"
+  },
+  {
+    "revision": "9d7bdfb93589aa107e61",
+    "url": "/corona-tracker/static/js/runtime-main.0c9ed856.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
